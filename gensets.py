f32c9518e6072751b1f8235f4a6018dbc94feef5
Kevin Zhang <hello@kevinzha.ng>
2019-09-18 18:49:41 -0400

    add script for generating set txt file

---
+++ gensets.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import requests
+
+r = requests.get("https://api.scryfall.com/sets").json()
+r = r['data']
+ignore = [
+        'fbb',
+        'sum'
+        ]
+for s in r:
+    if s['set_type'] == 'commander' or s['set_type'] == 'core' or s['set_type'] == 'expansion':
+        if s['code'] not in ignore:
+            print(s['code'])
+
