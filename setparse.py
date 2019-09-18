86a10eba14686e1ca0e2fc23344aa6961720a7e2
Kevin Zhang <hello@kevinzha.ng>
2019-09-18 16:21:49 -0400

    setparse as importable module and runnable

--- setparse.py
+++ setparse.py
@@ -4,22 +4,27 @@ import requests
 import sys
 from datetime import datetime
 
-more = True
-setdata=[]
-api = "https://api.scryfall.com/cards/search?q=set=" + sys.argv[1]
-while(more):
-    scrydata = requests.get(api).json()
-    more = scrydata['has_more']
-    if more:
-        api = scrydata['next_page']
-    setdata.extend(scrydata['data'])
-ima = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+def setparse(mtgset, output):
+    sys.stdout = output
+    more = True
+    setdata=[]
+    api = "https://api.scryfall.com/cards/search?q=set=" + mtgset
+    while(more):
+        scrydata = requests.get(api).json()
+        more = scrydata['has_more']
+        if more:
+            api = scrydata['next_page']
+        setdata.extend(scrydata['data'])
+    ima = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-def process(card):
-    card.update({"timestamp":ima})
-    return card
+    def process(card):
+        card.update({"timestamp":ima})
+        return card
 
-carddata = list(map(process, setdata))
+    carddata = list(map(process, setdata))
 
-for card in carddata:
-    print(json.dumps(card))
+    for card in carddata:
+        print(json.dumps(card))
+
+if __name__ == '__main__':
+    setparse(sys.argv[1], sys.__stdout__)
