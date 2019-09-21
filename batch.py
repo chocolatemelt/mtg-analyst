10f24fb830e08d8c8b73c7849f9fac9cd8c51285
Kevin Zhang <hello@kevinzha.ng>
2019-09-21 17:36:37 -0400

    add bzip2 functions for smoother rw

--- batch.py
+++ batch.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
-"""Batch downloads cards from sets given in the scryfall API.
-
-Retrieves all cards from the listed sets in sets.txt using the scryfall API,
-inserting the mandated 100ms delay between API calls to reduce server load
-on the scryfall side.
-
-sets.txt is formatted exclusively as a newline-delimited text file of set
-codes as noted under the key ['code'] from the scryfall set API.
-"""
+import bz2
+import requests
 import setparse
+import sys
+import tarfile
 import time
 from datetime import datetime
 
-if __name__ == '__main__':
+import config
+
+def batch_recv():
+    """Batch downloads cards from sets given in the scryfall API.
+
+    Retrieves all cards from the listed sets in sets.txt using the scryfall API,
+    inserting the mandated 100ms delay between API calls to reduce server load
+    on the scryfall side.
+
+    sets.txt is formatted exclusively as a newline-delimited text file of set
+    codes as noted under the key ['code'] from the scryfall set API.
+    """
     output = datetime.now().strftime('%Y-%m-%d') + '-dump.json'
     outf = open('data/' + output, 'w+')
     outf.truncate(0)
@@ -20,3 +26,40 @@ if __name__ == '__main__':
     for mtgset in f.readlines():
         setparse.setparse(mtgset, outf)
         time.sleep(1)
+
+def cfj(filename = None):
+    """Emulates tar cfj for a given file under the data folder.
+
+    Arguments:
+        filename
+            A named file under the data/ folder.
+    """
+    if not filename:
+        filename = datetime.now().strftime('%Y-%m-%d') + '-dump.json'
+    output = filename + '.tar.bz2'
+    with tarfile.open(output, 'w:bz2') as tar:
+        tar.add('data/' + filename, arcname = filename)
+
+def xf(filename = None):
+    """Emulates tar xf for a given tar.bz file.
+
+    Arguments:
+        filename
+            A named file.
+    """
+    if not filename:
+        filename = datetime.now().strftime('%Y-%m-%d') + '-dump.json.tar.bz2'
+    tar = tarfile.open(filename, 'r:bz2')
+    tar.extractall('data/')
+    tar.close()
+
+def up(filename):
+    files = { 'file': open(filename, 'rb') }
+    print(requests.post(config.api, files=files).text)
+
+def dl(filename):
+    r = requests.get(config.api + '?file=' + filename, allow_redirects=True)
+    open(filename, 'wb').write(r.content)
+
+if __name__ == '__main__':
+    dl(sys.argv[1])
