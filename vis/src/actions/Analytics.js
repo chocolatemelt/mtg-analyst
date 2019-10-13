2e298005d8488b9108a1bf9d4bee18f02fddd09e
Kevin Zhang <hello@kevinzha.ng>
2019-10-13 18:31:48 -0400

    convert actions to arrow functions

--- vis/src/actions/Analytics.js
+++ vis/src/actions/Analytics.js
@@ -2,23 +2,17 @@ export const FETCHING_SAMPL_DATA = 'FETCHING_SAMPL_DATA';
 export const RECV_SAMPL_DATA = 'RECV_SAMPL_DATA';
 export const FETCH_ERROR = 'FETCH_ERROR';
 
-export function fetchingSampleData(isFetching) {
-  return {
-    type: FETCHING_SAMPL_DATA,
-    isFetching,
-  };
-}
+export const fetchingSampleData = isFetching => ({
+  type: FETCHING_SAMPL_DATA,
+  isFetching,
+});
 
-export function recvSampleData(data) {
-  return {
-    type: RECV_SAMPL_DATA,
-    data,
-  };
-}
+export const recvSampleData = data => ({
+  type: RECV_SAMPL_DATA,
+  data,
+});
 
-export function fetchError(msg) {
-  return {
-    type: FETCH_ERROR,
-    msg,
-  };
-}
+export const fetchError = msg => ({
+  type: FETCH_ERROR,
+  msg,
+});
